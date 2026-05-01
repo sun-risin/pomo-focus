4a2212edd6b59f41d229902e9c24c6c85facf978
SunJu <ciwrssn10@naver.com>
2026-05-02 03:17:55 +0900

    feat: add event listener to add todo btn

--- static/script.js
+++ static/script.js
@@ -1,12 +1,28 @@
-import { startTimer, pauseTimer, resetTimer } from "./timer.js";
+import * as TimerModule from "./timer.js";
+import * as TodoModule from "./todo.js";
 
 // --- DOM 요소 참조 가져오기
+// 타이머 버튼
 const startBtn = document.getElementById('startBtn');
 const pauseBtn = document.getElementById('pauseBtn');
 const resetBtn = document.getElementById('resetBtn');
 
-// --- 이벤트 리스너
-// 타이머 버튼에 연결
-startBtn.addEventListener('click', startTimer);
-pauseBtn.addEventListener('click', pauseTimer);
-resetBtn.addEventListener('click', resetTimer);
+// todo 관련
+const todoList = document.getElementById('todoList');
+const todoInput = document.getElementById('todoInput');
+const activeTaskTitle = document.getElementById('activeTaskTitle');
+const addTodoBtn = document.getElementById('addTodoBtn');
+
+
+// --- 이벤트 리스너 연결 및 설정
+// 타이머 버튼
+startBtn.addEventListener('click', TimerModule.startTimer);
+pauseBtn.addEventListener('click', TimerModule.pauseTimer);
+resetBtn.addEventListener('click', TimerModule.resetTimer);
+
+// todo 추가 버튼
+addTodoBtn.addEventListener('click', () => {
+    TodoModule.addTodo(todoInput.value);
+    todoInput.value = '';
+    // todo 렌더링
+});
--- static/todo.js
+++ static/todo.js
@@ -1,21 +1,26 @@
 // --- 상태 관리 변수
-let todoList = [];
+let todos = [];
 let selectedTodoId = null;
 
-// --- 내부 변수 get, set : export
-export function getTodoList(){ // todos는 읽기만 필요
-    return todoList;
-}
 
+// 이하 모두 export
+// --- 내부 변수 get
+export function getTodos(){ // 복사본 반환 (원본 배열 보호)
+    return [...todos];
+}
 export function getSelectedTodoId(){
     return selectedTodoId;
 }
+export function getSelectedTodos(){
+    return todos.find(todo => todo.id === selectedTodoId) ?? null;
+}
 
+// --- 내부 set
 export function setSelectedTodoId(id) {
     selectedTodoId = id;
 }
 
-// --- todo 제어 : export
+// --- todo 제어
 // todo 추가
 export function addTodo(title) {
     const newTodo = {
@@ -24,15 +29,17 @@ export function addTodo(title) {
         completed: false,
         totalTime: 0
     };
-    todoList.append(newTodo);
-    return newTodo;
+    todos.push(newTodo);
 }
 
 // todo 완료 처리
-export function toggleTaskComplete(id, isChecked) {
-    const todo = todoList.find(t => t.id === id);
-    if (todo) {
-        todo.completed = isChecked;
-    }
-    return todo;
+export function toggleTaskComplete(id) {
+    const todo = todos.find(t => t.id === id);
+    if (todo) todo.completed = !todo.completed;
+}
+
+// todo 수행 시 걸린 시간 증가 처리
+export function incrementTakenTime(id){
+    const todo = todos.find(t => t.id === id);
+    if (todo) todo.totalTime++;
 }
